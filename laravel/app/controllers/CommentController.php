bb47c9b0a2153b8d404429c6ee2f9c219042244b
Woradej Kanokwannarat <woradej.guide@gmail.com>
2014-11-19 01:09:52 +0700

    rate edit

--- laravel/app/controllers/CommentController.php
+++ laravel/app/controllers/CommentController.php
@@ -13,15 +13,14 @@ class CommentController extends BaseController {
             return Redirect::to('recipe/show/'.$recipe->id)->withErrors($validator);
         }else{
             $score = Input::get('score');
-            return var_dump($score);
             $user = User::find(Auth::user()->id);
             $text = Input::get('comment');
 
             $comment = new Comment;
-            $comment->comment($text);
-            $comment->setUserId($user->id);
-            $comment->SetRecipeId($recipe->id);
-            $comment->newComment();
+            $comment->comment=$text;
+            $comment->user_id=$user->id;
+            $comment->recipe_id=$recipe->id;
+            $comment->save();
 
             $rate = Rate::create(array(
                 'score'         => $score,
@@ -31,11 +30,11 @@ class CommentController extends BaseController {
 
             if($rate){
                 $recipe->score->update(array('rateCount' => $recipe->score->rateCount + 1));
-            }
-            $newscore = $rate->score + $recipe->score->scoreSum;
-
-            $recipe->score->update(array('scoreSum'  => $newscore));
-
+            } 
+            $rscore=$recipe->score->get();
+            $newscore = $rate->score + $rscore->scoreSum;
+        
+            $rscore->update(array('scoreSum'  => $newscore));
 
             return Redirect::to('recipe/show/'.$recipe->id);
         }
